6b60e11ace460af3754fa75ad194fee422232c30
Hiraparavishal <vishuhirapara@gmail.com>
2020-09-01 15:32:50 +0530

    login Signup added

---
+++ frontend/src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+var firebaseConfig = {
+  apiKey: "AIzaSyApIteuzYbig9PziNQjWl0qf0BOkR0OWQg",
+  authDomain: "finalyear-d27ea.firebaseapp.com",
+  databaseURL: "https://finalyear-d27ea.firebaseio.com",
+  projectId: "finalyear-d27ea",
+  storageBucket: "finalyear-d27ea.appspot.com",
+  messagingSenderId: "215093813377",
+  appId: "1:215093813377:web:b178cbad014923b3a3ed80",
+  measurementId: "G-GZPYWB5076",
+};
+firebase.initializeApp(firebaseConfig);
+export default firebaseConfig;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import Login from "./component/login/Login";
+import Signup from "./component/signUp/SignUp";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import "firebase";
+import "../../frontend/src/firebase";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Router>
+    <Switch>
+      <Route path="/signup" exact component={Signup}></Route>
+      <Route path="/login" exact component={Login}></Route>
+    </Switch>
+  </Router>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// Learn more about service workers: https://bit.ly/CRA-PWAl
 serviceWorker.unregister();
